4773fdfe7a47653b49e4dcc14608875ab050ad8c
rhit-leey2 <73076474+rhit-leey2@users.noreply.github.com>
2021-04-20 15:25:00 -0400

    changes to duration

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@
 	    new ScrollMagic.Scene({
 	            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
 	            offset: 50, // start a little later
-	            duration: "80%",
+	            duration: "75%",
 	            triggerHook: 0.9,
 	        })
 	        .setClassToggle(revealElements[i], "visible") // add class toggle
@@ -22,8 +22,8 @@
 	for (var i = 0; i < revealElements.length; i++) { // create a scene for each element
 	    new ScrollMagic.Scene({
 	            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-	            offset: 40, // start a little later
-	            duration: "80%",
+	            offset: 60, // start a little later
+	            duration: "70%",
 	            triggerHook: 0.9,
 	        })
 	        .setClassToggle(revealElements[i], "visible") // add class toggle
