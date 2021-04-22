5beed5d3bfbae8e2cc307be7c6a5c5ba1785b3cd
rhit-leey2 <73076474+rhit-leey2@users.noreply.github.com>
2021-04-21 23:49:07 -0400

    final

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,49 @@
 	var controller = new ScrollMagic.Controller();
+/*
+    if ($(window).width() > 768) {
+        var element = document.getElementById("pinn");
+        element.id = "pin2";
+    }else{
+        var element = document.getElementById("pin2");
+        element.id = "pinn";
+    } 
 
-	var revealElements = document.getElementsByClassName("portItems");
-	for (var i = 0; i < revealElements.length; i++) { // create a scene for each element
+    var pinItems = document.getElementsByClassName("pinItems");
+    for (var i = 0; i < pinItems.length; i++) {
+        new ScrollMagic.Scene({
+        triggerElement: "#trigger2"
+      })
+      .setPin("#pin2")
+      .addIndicators({name: "1 (duration: 0)"}) 
+      .addTo(controller);
+    } */
+
+    var revealItems = document.getElementsByClassName("aboutItems");
+	for (var i = 0; i < revealItems.length; i++) { // create a scene for each element
 	    new ScrollMagic.Scene({
-	            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-	            offset: 50, // start a little later
+	            triggerElement: revealItems[i], // y value not modified, so we can use element as trigger as well
+	            offset: 40, // start a little later
+	            duration: "80%",
+	            triggerHook: 0.9,
+	        })
+	        .setClassToggle(revealItems[i], "visible") // add class toggle
+	        /*
+            .addIndicators({
+	            name: "aboutItems " + (i + 1)
+	        }) // add indicators (requires plugin)
+            */
+	        .addTo(controller);
+	}
+
+	var revealItems = document.getElementsByClassName("portItems");
+	for (var i = 0; i < revealItems.length; i++) { // create a scene for each element
+	    new ScrollMagic.Scene({
+	            triggerElement: revealItems[i], // y value not modified, so we can use element as trigger as well
+	            offset: 60, // start a little later
 	            duration: "75%",
 	            triggerHook: 0.9,
 	        })
-	        .setClassToggle(revealElements[i], "visible") // add class toggle
+	        .setClassToggle(revealItems[i], "visible") // add class toggle
 	        /*
             .addIndicators({
 	            name: "portItems " + (i + 1)
@@ -18,15 +53,15 @@
 	}
 
 
-	var revealElements = document.getElementsByClassName("resItems");
-	for (var i = 0; i < revealElements.length; i++) { // create a scene for each element
+	var revealItems = document.getElementsByClassName("resItems");
+	for (var i = 0; i < revealItems.length; i++) { // create a scene for each element
 	    new ScrollMagic.Scene({
-	            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+	            triggerElement: revealItems[i], // y value not modified, so we can use element as trigger as well
 	            offset: 60, // start a little later
 	            duration: "70%",
 	            triggerHook: 0.9,
 	        })
-	        .setClassToggle(revealElements[i], "visible") // add class toggle
+	        .setClassToggle(revealItems[i], "visible") // add class toggle
 	        /*
 	        .addIndicators({
 	            name: "resItems " + (i + 1)
@@ -34,3 +69,4 @@
             */
 	        .addTo(controller);
 	}
+
